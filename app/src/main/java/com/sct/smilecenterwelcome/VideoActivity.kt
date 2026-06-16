d1580577e62ea95e197eb384c134af2ea5c30c8b
gerinbaris <gerinbaris@gmail.com>
2026-06-16 19:10:25 +0300

    Update VideoActivity.kt

--- app/src/main/java/com/sct/smilecenterwelcome/VideoActivity.kt
+++ app/src/main/java/com/sct/smilecenterwelcome/VideoActivity.kt
@@ -4,7 +4,6 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.view.WindowManager
-import android.webkit.JavascriptInterface
 import android.webkit.WebChromeClient
 import android.webkit.WebView
 import android.webkit.WebViewClient
@@ -27,40 +26,17 @@ class VideoActivity : AppCompatActivity() {
         webView.settings.javaScriptEnabled = true
         webView.settings.mediaPlaybackRequiresUserGesture = false
         webView.settings.domStorageEnabled = true
+        webView.settings.loadWithOverviewMode = true
+        webView.settings.useWideViewPort = true
         webView.webChromeClient = WebChromeClient()
         webView.webViewClient = WebViewClient()
-        webView.addJavascriptInterface(VideoJSInterface(), "Android")
 
-        val html = """
-            <!DOCTYPE html><html><head>
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <style>*{margin:0;padding:0;background:#000} body{width:100vw;height:100vh;overflow:hidden} #player{width:100%;height:100%;position:fixed;top:0;left:0}</style>
-            </head><body>
-            <div id="player"></div>
-            <script>
-            var tag=document.createElement('script');
-            tag.src='https://www.youtube.com/iframe_api';
-            document.head.appendChild(tag);
-            var player;
-            function onYouTubeIframeAPIReady(){
-                player=new YT.Player('player',{
-                    videoId:'$VIDEO_ID',
-                    width:'100%',height:'100%',
-                    playerVars:{autoplay:1,controls:0,rel:0,modestbranding:1,playsinline:1},
-                    events:{
-                        onReady:function(e){e.target.playVideo();},
-                        onStateChange:function(e){if(e.data===0)Android.onVideoEnded();},
-                        onError:function(e){Android.onVideoEnded();}
-                    }
-                });
-            }
-            </script></body></html>
-        """.trimIndent()
-
-        webView.loadDataWithBaseURL("https://www.youtube.com", html, "text/html", "UTF-8", null)
+        val url = "https://www.youtube.com/embed/$VIDEO_ID?autoplay=1&controls=1&rel=0&modestbranding=1&playsinline=1"
+        webView.loadUrl(url)
     }
 
     fun goToWelcome() {
+        webView.loadUrl("about:blank")
         val intent = Intent(this, MainActivity::class.java)
         intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
         startActivity(intent)
@@ -73,9 +49,4 @@ class VideoActivity : AppCompatActivity() {
 
     @Deprecated("Deprecated in Java")
     override fun onBackPressed() { goToWelcome() }
-
-    inner class VideoJSInterface {
-        @JavascriptInterface
-        fun onVideoEnded() { runOnUiThread { goToWelcome() } }
-    }
 }
