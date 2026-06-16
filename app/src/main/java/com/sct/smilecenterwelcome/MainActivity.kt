bc1e0210a3ae42e84eac00af4fa9756e701317eb
gerinbaris <gerinbaris@gmail.com>
2026-06-16 19:04:35 +0300

    Update MainActivity.kt

--- app/src/main/java/com/sct/smilecenterwelcome/MainActivity.kt
+++ app/src/main/java/com/sct/smilecenterwelcome/MainActivity.kt
@@ -1,80 +1,47 @@
 package com.sct.smilecenterwelcome
 
 import android.content.Intent
-import android.os.Build
 import android.os.Bundle
-import android.os.CountDownTimer
-import android.view.View
-import android.view.WindowInsets
-import android.view.WindowInsetsController
+import android.os.Handler
+import android.os.Looper
 import android.view.WindowManager
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
-    private var countDownTimer: CountDownTimer? = null
+    private val handler = Handler(Looper.getMainLooper())
+    private var secondsLeft = 5
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
-        setFullScreen()
-
+        window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        supportActionBar?.hide()
         setContentView(R.layout.activity_main)
+        startCountdown()
+    }
 
+    private fun startCountdown() {
         val tvCountdown = findViewById<TextView>(R.id.tvCountdown)
-
-        countDownTimer = object : CountDownTimer(6000, 1000) {
-            override fun onTick(millisUntilFinished: Long) {
-                val secondsLeft = (millisUntilFinished / 1000).toInt()
+        handler.post(object : Runnable {
+            override fun run() {
                 if (secondsLeft > 0) {
                     tvCountdown.text = "Video başlıyor... $secondsLeft"
+                    secondsLeft--
+                    handler.postDelayed(this, 1000)
+                } else {
+                    startActivity(Intent(this@MainActivity, VideoActivity::class.java))
                 }
             }
-
-            override fun onFinish() {
-                try {
-                    val intent = Intent(this@MainActivity, VideoActivity::class.java)
-                    startActivity(intent)
-                    overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
-                } catch (e: Exception) {
-                e.printStackTrace()
-                }
-        }
-        }.start()
-    }
-
-    private fun setFullScreen() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-            window.insetsController?.let { controller ->
-                controller.hide(WindowInsets.Type.statusBars() or WindowInsets.Type.navigationBars())
-                controller.systemBarsBehavior =
-                    WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
-            }
-        } else {
-            @Suppress("DEPRECATION")
-            window.decorView.systemUiVisibility = (
-                View.SYSTEM_UI_FLAG_FULLSCREEN or
-                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
-                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-            )
-        }
-    }
-
-    override fun onResume() {
-        super.onResume()
-        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
-        setFullScreen()
+        })
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        countDownTimer?.cancel()
+        handler.removeCallbacksAndMessages(null)
     }
 
     @Deprecated("Deprecated in Java")
-    override fun onBackPressed() {
-        // TV'de yanlışlıkla kapanmayı önle
-    }
+    override fun onBackPressed() { }
 }
